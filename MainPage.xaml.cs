6b9edfbd8838f9a1fdf3817cc073962a4e689679
Gaurang Naik <A00250808@mycambrian.ca>
2023-05-22 18:08:31 -0400

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace LoginAttempts;
+
+public partial class MainPage : ContentPage
+{
+	string password = "welcome";
+	int retryCount = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnClicked(object sender, EventArgs e)
+	{
+	 if(txtbxPassword.Text.ToLower().Equals(password))
+		{
+			lblStatus.Text = "Welcome !";
+		}
+	 else
+		{
+			retryCount++;
+			if (retryCount >= 5)
+			{
+                lblStatus.Text = "You have been locked out.";
+            }
+			else
+				lblStatus.Text = "Incorrect password.";
+
+
+        }
+		//if (count == 1)
+		//	CounterBtn.Text = $"Clicked {count} time";
+		//else
+		//	CounterBtn.Text = $"Clicked {count} times";
+
+	//	SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
