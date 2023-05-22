2702e421a0a5465ad3a1fa13af6ed6b0d5daf760
Gaurang Naik <A00250808@mycambrian.ca>
2023-05-22 18:13:22 -0400

    Updated code

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -28,12 +28,6 @@ public partial class MainPage : ContentPage
 
 
         }
-		//if (count == 1)
-		//	CounterBtn.Text = $"Clicked {count} time";
-		//else
-		//	CounterBtn.Text = $"Clicked {count} times";
-
-	//	SemanticScreenReader.Announce(CounterBtn.Text);
 	}
 }
 
